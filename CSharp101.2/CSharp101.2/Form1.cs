0e4434621c0f8f4379f337c8b1c3737468729352
NewtonGaliza <newtonjgaliza@gmail.com>
2017-03-13 23:25:59 -0300

    one button to verify one checkbox

---
+++ CSharp101.2/CSharp101.2/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CSharp101._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!checkBox1.Checked)
+            {
+                MessageBox.Show("Check the CheckBox", "Checked?");
+            }
+            else if (checkBox1.Checked)
+            {
+                MessageBox.Show("CheckBox is checked", "Checked?");
+            }
+        }
+    }
+}
